f434ca45ee962647e73fdffa928007d15d8ed6e4
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-03-04 20:25:52 +0100

    chore: config routes, mock data and metadata :technologist:

---
+++ constants/routes.ts
@@ -0,0 +1,13 @@
+
+const HOME_URL: string = '/'
+
+const ABOUT_URL: string = '/about'
+const CHURCHES_URL: string = '/churches'
+const CLERGY_URL: string = '/clergy'
+const EVENTS_URL: string = '/events'
+const YOUTH_URL: string = '/youth'
+const MEDIA_URL: string = '/media'
+const NEWS_URL: string = '/news'
+const CONTACT_URL: string = '/contact'
+
+export { HOME_URL, ABOUT_URL, CHURCHES_URL, CLERGY_URL, EVENTS_URL, YOUTH_URL, MEDIA_URL, NEWS_URL, CONTACT_URL }
---
+++ data/index.ts
@@ -0,0 +1,16 @@
+import { ABOUT_URL, CHURCHES_URL, CLERGY_URL, EVENTS_URL, YOUTH_URL, MEDIA_URL, NEWS_URL, CONTACT_URL, HOME_URL } from "@/constants/routes";
+
+const navLinks = [
+  { href: HOME_URL, label: "Home" },
+  { href: ABOUT_URL, label: "About" },
+  { href: CHURCHES_URL, label: "Churches" },
+  { href: CLERGY_URL, label: "Clergy" },
+  { href: EVENTS_URL, label: "Events" },
+  { href: YOUTH_URL, label: "Youth" },
+  { href: MEDIA_URL, label: "Media" },
+  { href: NEWS_URL, label: "News" },
+  { href: CONTACT_URL, label: "Contact" },
+];
+
+
+export { navLinks };
